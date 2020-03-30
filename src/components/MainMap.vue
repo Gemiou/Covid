360c9616661bfdad33f27fdb4512d45a335cf4a6
George Efthymiou <efthymiouge@gmail.com>
2020-03-30 12:00:30 +0300

    first commit

---
+++ src/components/MainMap.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "MainMap"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainStats.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "MainStats"
+    }
+</script>
+
+<style scoped>
+
+</style>
